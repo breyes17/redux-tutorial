9df10397d17d326a4fdd97984d1eb583a019ba7b
Bryan P. Reyes <bryanpr17@gmail.com>
2022-07-12 08:46:33 +0800

    consume the action creator in hooks component

--- src/component/Hooks.jsx
+++ src/component/Hooks.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { ADD_POINT, DEDUCTION } from '../redux/hooks/constant';
+import { addPoint, deduction } from '../redux/hooks/action';
 
 const Hooks = () => {
   const { score } = useSelector((state) => state.hooks);
   const dispatch = useDispatch();
 
-  const onDispatch = (type, points) => dispatch({ type, payload: points });
   return (
     <div>
       <p>Your score: {score}</p>
-      <button onClick={() => onDispatch(ADD_POINT, 2)}>2 point</button>
-      <button onClick={() => onDispatch(ADD_POINT, 3)}>3 point</button>
-      <button onClick={() => onDispatch(DEDUCTION, 1)}>Deduction</button>
+      <button onClick={() => dispatch(addPoint(2))}>2 point</button>
+      <button onClick={() => dispatch(addPoint(3))}>3 point</button>
+      <button onClick={() => dispatch(deduction(1))}>Deduction</button>
     </div>
   );
 };
